c07352cd627839d61e3e1b34dfe2863bf29c9ca1
danbarcan <dan.barcan1994@gmail.com>
2018-12-22 12:45:51 +0200

    email sender

---
+++ src/main/java/com/dans/service/services/MailService.java
@@ -0,0 +1,87 @@
+package com.dans.service.services;
+
+import org.springframework.stereotype.Service;
+
+import javax.mail.*;
+import javax.mail.internet.*;
+import java.io.IOException;
+import java.util.Date;
+import java.util.Properties;
+
+@Service
+public class MailService {
+
+    public void sendmail() throws AddressException, MessagingException, IOException {
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+
+        Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication("dan.barcan1994", "******");
+            }
+        });
+        Message msg = new MimeMessage(session);
+        msg.setFrom(new InternetAddress("welcome@serviceulmeu.ro", "Serviceul meu"));
+
+        msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse("dan.barcan1994@gmail.com"));
+        msg.setSubject("Bine ai venit!");
+        msg.setContent("Bine ai venit la <b>Serviceul meu</b>", "text/html");
+        msg.setSentDate(new Date());
+
+        MimeBodyPart messageBodyPart = new MimeBodyPart();
+        messageBodyPart.setContent("Bine ai venit la <b>Serviceul meu</b>\"", "text/html");
+
+        Multipart multipart = new MimeMultipart();
+        multipart.addBodyPart(messageBodyPart);
+        msg.setContent(multipart);
+        Transport.send(msg);
+    }
+
+    public void sendmail(String from, String pass, String[] to, String subject, String body) {
+        sendFromGMail(from, pass, to, subject, body);
+    }
+
+    private static void sendFromGMail(String from, String pass, String[] to, String subject, String body) {
+        Properties props = System.getProperties();
+        String host = "smtp.gmail.com";
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", host);
+        props.put("mail.smtp.user", from);
+        props.put("mail.smtp.password", pass);
+        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.auth", "true");
+
+        Session session = Session.getDefaultInstance(props);
+        MimeMessage message = new MimeMessage(session);
+
+        try {
+            message.setFrom(new InternetAddress(from));
+            InternetAddress[] toAddress = new InternetAddress[to.length];
+
+            // To get the array of addresses
+            for( int i = 0; i < to.length; i++ ) {
+                toAddress[i] = new InternetAddress(to[i]);
+            }
+
+            for( int i = 0; i < toAddress.length; i++) {
+                message.addRecipient(Message.RecipientType.TO, toAddress[i]);
+            }
+
+            message.setSubject(subject);
+            message.setText(body);
+            Transport transport = session.getTransport("smtp");
+            transport.connect(host, from, pass);
+            transport.sendMessage(message, message.getAllRecipients());
+            transport.close();
+        }
+        catch (AddressException ae) {
+            ae.printStackTrace();
+        }
+        catch (MessagingException me) {
+            me.printStackTrace();
+        }
+    }
+}
