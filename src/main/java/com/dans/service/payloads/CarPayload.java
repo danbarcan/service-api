2c16aecef1ecac8ced58455c3e52d121f958d1db
danbarcan <dan.barcan1994@gmail.com>
2019-01-14 00:10:50 +0200

    carpayload field annotation fix

--- src/main/java/com/dans/service/payloads/CarPayload.java
+++ src/main/java/com/dans/service/payloads/CarPayload.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Data
@@ -24,6 +25,6 @@ public class CarPayload {
     @Size(min = 4, max = 4)
     private String year;
 
-    @NotBlank
+    @NotNull
     private Long userId;
 }
