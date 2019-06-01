3235aec9d8c96b95731ad120f67c4f002b3fd3a7
Dan Barcan <dan.barcan1994@gmail.com>
2019-06-02 00:01:50 +0300

    fix chat

--- src/main/java/com/dans/service/repositories/JobRepository.java
+++ src/main/java/com/dans/service/repositories/JobRepository.java
@@ -1,6 +1,7 @@
 package com.dans.service.repositories;
 
 import com.dans.service.entities.Job;
+import com.dans.service.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
@@ -15,5 +16,5 @@ public interface JobRepository extends JpaRepository<Job, Long> {
 
     List<Job> findAllByUserIdOrderByTimestampDesc(Long userId);
 
-    List<Job> findAllByUserIdOrAcceptedServiceOrderByTimestampDesc(Long userId, Long acceptedServiceId);
+    List<Job> findAllByUserAndAcceptedServiceOrderByTimestampDesc(User user, User acceptedService);
 }
