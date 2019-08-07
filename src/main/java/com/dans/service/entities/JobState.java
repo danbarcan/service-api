d457b95194bd043d97ac58834d47b68dd931db41
Dan Barcan <dan.barcan1994@gmail.com>
2019-08-08 01:56:53 +0300

    review functionality for job state

--- src/main/java/com/dans/service/entities/JobState.java
+++ src/main/java/com/dans/service/entities/JobState.java
@@ -1,7 +1,7 @@
 package com.dans.service.entities;
 
 public enum JobState {
-    AVAILABLE, UNAVAILABLE, HIDDEN, OFFERED, ACCEPTED;
+    AVAILABLE, UNAVAILABLE, HIDDEN, OFFERED, ACCEPTED, COMPLETED;
 
     public static JobState getState(Job job, User user) {
         if (job.getAcceptedService() != null) {
@@ -11,7 +11,9 @@ public enum JobState {
                 return UNAVAILABLE;
             }
         } else {
-            if (job.getOffers().stream().anyMatch(offer -> user.equals(offer.getUser()))) {
+            if (job.getReview() != null) {
+                return COMPLETED;
+            } else if (job.getOffers().stream().anyMatch(offer -> user.equals(offer.getUser()))) {
                 return OFFERED;
             } else if (job.getHiddenForUsers().contains(user)) {
                 return HIDDEN;
