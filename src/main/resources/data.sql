90f20006c20797fe9bd5d4a7b1f00bb1309aa8f9
Dan Barcan <dan.barcan1994@gmail.com>
2019-12-18 23:43:55 +0200

    more categories

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,5 +2,16 @@ INSERT INTO roles(name) VALUES('ROLE_USER');
 INSERT INTO roles(name) VALUES('ROLE_SERVICE');
 INSERT INTO roles(name) VALUES('ROLE_ADMIN');
 
-insert into categories(id, description) values (0, 'cat1');
-insert into categories(id, description) values (1, 'cat2');
+insert into categories(id, description) values (0, 'Revizie Auto');
+insert into categories(id, description) values (1, 'Sistem de franare');
+insert into categories(id, description) values (2, 'Distributie');
+insert into categories(id, description) values (3, 'Ambreiaj');
+insert into categories(id, description) values (4, 'Suspensie');
+insert into categories(id, description) values (5, 'Directie');
+insert into categories(id, description) values (6, 'Reparatii motor');
+insert into categories(id, description) values (7, 'Service Roti');
+insert into categories(id, description) values (8, 'Transmisie');
+insert into categories(id, description) values (9, 'Suspensie');
+insert into categories(id, description) values (10, 'Turbina');
+insert into categories(id, description) values (11, 'Altele');
+insert into categories(id, description) values (12, 'Toate de mai sus');
